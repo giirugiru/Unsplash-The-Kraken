7b14cf2d2d58480d959d3fa075c2362b486eb791
Gilang Sinawang <giirugiru@gmail.com>
2020-11-27 12:12:05 +0700

    Initial Commit

---
+++ Unsplash The Kraken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unsplash The Kraken
+//
+//  Created by Gilang Sinawang on 27/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
